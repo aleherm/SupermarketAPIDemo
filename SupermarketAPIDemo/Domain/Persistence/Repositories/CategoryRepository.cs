1fc3771498961d3b8a1b45e13ec921745345be34
Alexandra Hermeneanu <ahermeneanu@euoutsourcing.com>
2020-10-27 15:17:01 +0200

    added dependency injection

--- SupermarketAPIDemo/Domain/Persistence/Repositories/CategoryRepository.cs
+++ SupermarketAPIDemo/Domain/Persistence/Repositories/CategoryRepository.cs
@@ -1,7 +1,8 @@
-﻿using SupermarketAPIDemo.Domain.Persistence.Contexts;
-using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketAPIDemo.Domain.Models;
+using SupermarketAPIDemo.Domain.Persistence.Contexts;
+using SupermarketAPIDemo.Domain.Repositories;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SupermarketAPIDemo.Domain.Persistence.Repositories
--- SupermarketAPIDemo/Startup.cs
+++ SupermarketAPIDemo/Startup.cs
@@ -1,34 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using SupermarketAPIDemo.Domain.Persistence.Contexts;
+using SupermarketAPIDemo.Domain.Persistence.Repositories;
+using SupermarketAPIDemo.Domain.Repositories;
+using SupermarketAPIDemo.Domain.Services;
+using SupermarketAPIDemo.Services;
 
 namespace SupermarketAPIDemo
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
+        }
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
