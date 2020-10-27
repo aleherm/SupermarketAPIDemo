ea550d1b5a99c02c1933b78cd0c9f0fb2284c76f
Alexandra Hermeneanu <ahermeneanu@euoutsourcing.com>
2020-10-27 14:42:53 +0200

    created API project with Models and Controllers

---
+++ SupermarketAPIDemo/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupermarketAPIDemo.Domain.Models;
+using SupermarketAPIDemo.Domain.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupermarketAPIDemo.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
+    }
+}
---
+++ SupermarketAPIDemo/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using SupermarketAPIDemo.Domain.Models;
+using SupermarketAPIDemo.Domain.Repositories;
+using SupermarketAPIDemo.Domain.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupermarketAPIDemo.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _categoryRepository.ListAsync();
+        }
+    }
+}
